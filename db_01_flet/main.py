df6e337098a96628c980bc29c868b6547b1745b9
roamming-123 <cvn0200@gmail.com>
2026-05-07 17:25:20 +0900

    🎉프로젝트 시작

--- db_01_flet/main.py
+++ db_01_flet/main.py
@@ -3,15 +3,7 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    # page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-    # def on_btn_click(e):
-    #     # btn_change.content = '눌렸습니다'
-    #     text_counter.value = '반갑습니다'
-    #     page.update()
-
-    # text_counter = ft.Text('안녕하세요')
-    # btn_change = ft.Button('눌러 주세요', on_click=on_btn_click)
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     text_counter = ft.Text(
         value='0',
@@ -25,16 +17,30 @@ def main(page: ft.Page):
 
     def plus_click(e):
         text_counter.value = str(int(text_counter.value) + 1)
-
     page.add(
-        ft.Row(
-            alignment=ft.MainAxisAlignment.CENTER,
-            controls = [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                text_counter,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
-            ]
-        )
+        ft.Container (
+            content=ft.Row(
+                # alignment=ft.MainAxisAlignment.CENTER,
+                vertical_alignment=ft.CrossAxisAlignment.START,
+            controls=[
+                    ft.Container(
+                        content=ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                        bgcolor=ft.Colors.RED_400,
+                    ),
+                    ft.Container(
+                        content=text_counter,
+                        bgcolor=ft.Colors.BLUE_400,
+                    ),
+                    ft.Container(
+                        content=ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+                        bgcolor=ft.Colors.GREEN_700,
+                    ),
+                ],
+            ),
+        bgcolor=ft.Colors.YELLOW_900,
     )
+)
+        
 
+if__name__ = "main"
 ft.run(main)
--- db_01_flet/tempCodeRunnerFile.py
+++ db_01_flet/tempCodeRunnerFile.py
@@ -1,15 +1,2 @@
-import flet as ft
-def main(page: ft.Page):
-    page.title = "카운터";
-    page.add(
-        ft.Row(
-            controls=
-            [ft.Text('안녕하세요'),
-             ft.Button('눌러주세요')
-            ]
-    )
-    )
-    
-
-if __name__ == "__main__":
-    ft.run(main)
+    alignment=ft.MainAxisAlignment.CENTER,
+            
